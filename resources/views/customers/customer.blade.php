85c9dd3af9859179bba778193a63a551b49aa30d
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-01-28 15:16:07 +0800

    Added content sidebar on selected customer pages
    This is to accommodate the Account Receivable Indicator (Active and Overdue) as shown on Slide 17 of the project presentation.

--- resources/views/customers/customer.blade.php
+++ resources/views/customers/customer.blade.php
@@ -5,40 +5,89 @@
 @endpush
 
 @section('content')
-{{-- Button Group Navigation --}}
-<div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-new-customer">
-        <span class="icon text-white-50">
-            <i class="fas fa-pen"></i>
-        </span>
-        <span class="text">New</span>
-    </button>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-file-import"></i>
-        </span>
-        <span class="text">Import</span>
-    </button>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-download"></i>
-        </span>
-        <span class="text">Export</span>
-    </button>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-download"></i>
-        </span>
-        <span class="text">Download Excel Format</span>
-    </button>    
-</div>
 
-{{-- Page Content --}}
-<div class="card">
-    <div class="card-body">
-        <h1>Customers > Customer</h1>
+<div class="row">
+
+    {{-- Main Content Section --}}
+    <div class="col-xl-10 col-lg-9 col-12">
+        {{-- Button Group Navigation --}}
+        <div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-new-customer">
+                <span class="icon text-white-50">
+                    <i class="fas fa-pen"></i>
+                </span>
+                <span class="text">New</span>
+            </button>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-file-import"></i>
+                </span>
+                <span class="text">Import</span>
+            </button>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-download"></i>
+                </span>
+                <span class="text">Export</span>
+            </button>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-download"></i>
+                </span>
+                <span class="text">Download Excel Format</span>
+            </button>    
+        </div>
+
+        {{-- Page Content --}}
+        <div class="card">
+            <div class="card-body">
+                <h1>Customers > Customer</h1>
+            </div>
+        </div>
+    </div>
+
+    {{-- Sidebar Content Section --}}
+    <div class="col-xl-2 col-lg-3 d-md-none d-lg-block">
+        <h4 class="">Account Receivable</h4>
+        {{-- Account Receivable Active --}}
+        <div class="mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Birr 40,000</div>
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                2 Active</div>
+                        </div>
+                        <div class="col-auto">
+                            {{-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- Account Receivable Overdue --}}
+        <div class="mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Birr 215,000</div>
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                5 Over Due</div>
+                        </div>
+                        <div class="col-auto">
+                            {{-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
+
+
 
 {{-- Modals --}}
 {{-- New Receipt --}}
--- resources/views/customers/receipt.blade.php
+++ resources/views/customers/receipt.blade.php
@@ -9,65 +9,115 @@
 @endpush
 
 @section('content')
-{{-- Button Group Navigation --}}
-<div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
-    <div class="btn-group" role="group">
-        <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="icon text-white-50">
-                <i class="fas fa-pen"></i>
-            </span>
-            <span class="text">New</span>
-        </button>
-        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
-            <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-receipt">Receipt</a>
-            <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-advance-revenue">Advance Revenue</a>
-            <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-credit-receipt">Credit Receipt</a>
-            <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-proforma">Proforma</a>
+
+<div class="row">
+
+    {{-- Main Content Section --}}
+    <div class="col-xl-10 col-lg-9 col-12">
+        {{-- Button Group Navigation --}}
+        <div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
+            <div class="btn-group" role="group">
+                <button id="btnGroupDrop1" type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="icon text-white-50">
+                        <i class="fas fa-pen"></i>
+                    </span>
+                    <span class="text">New</span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                    <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-receipt">Receipt</a>
+                    <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-advance-revenue">Advance Revenue</a>
+                    <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-credit-receipt">Credit Receipt</a>
+                    <a class="dropdown-item" href="javascript:void(0)" type="button" data-toggle="modal" data-target="#modal-new-proforma">Proforma</a>
+                </div>
+            </div>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-file-import"></i>
+                </span>
+                <span class="text">Import</span>
+            </button>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-download"></i>
+                </span>
+                <span class="text">Export</span>
+            </button>
+            <button type="button" class="btn btn-secondary">
+                <span class="icon text-white-50">
+                    <i class="fas fa-download"></i>
+                </span>
+                <span class="text">Download Excel Format</span>
+            </button>    
+        </div>
+
+        {{-- Tab Navigation --}}
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+            <li class="nav-item" role="presentation">
+                <a class="nav-link active" id="transactions-tab" data-toggle="tab" href="#transactions" role="tab" aria-controls="transactions" aria-selected="true">Transactions</a>
+            </li>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="proforma-tab" data-toggle="tab" href="#proforma" role="tab" aria-controls="proforma" aria-selected="false">Proforma</a>
+            </li>
+        </ul>
+
+        {{-- Tab Contents --}}
+        <div class="card" id="content-card">
+            <div class="card-body tab-content" id="myTabContent">
+                {{-- Transaction Contents --}}
+                <div class="tab-pane fade show active" id="transactions" role="tabpanel" aria-labelledby="transactions-tab">
+                    <h1>Transaction Contents</h1>
+                </div>
+                {{-- Proforma Contents --}}
+                <div class="tab-pane fade" id="proforma" role="tabpanel" aria-labelledby="proforma-tab">
+                    <h1>Proforma Contents</h1>
+                </div>
+            </div>
         </div>
     </div>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-file-import"></i>
-        </span>
-        <span class="text">Import</span>
-    </button>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-download"></i>
-        </span>
-        <span class="text">Export</span>
-    </button>
-    <button type="button" class="btn btn-secondary">
-        <span class="icon text-white-50">
-            <i class="fas fa-download"></i>
-        </span>
-        <span class="text">Download Excel Format</span>
-    </button>    
-</div>
 
-{{-- Tab Navigation --}}
-<ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="transactions-tab" data-toggle="tab" href="#transactions" role="tab" aria-controls="transactions" aria-selected="true">Transactions</a>
-    </li>
-    <li class="nav-item" role="presentation">
-        <a class="nav-link" id="proforma-tab" data-toggle="tab" href="#proforma" role="tab" aria-controls="proforma" aria-selected="false">Proforma</a>
-    </li>
-</ul>
-
-{{-- Tab Contents --}}
-<div class="card" id="content-card">
-    <div class="card-body tab-content" id="myTabContent">
-        {{-- Transaction Contents --}}
-        <div class="tab-pane fade show active" id="transactions" role="tabpanel" aria-labelledby="transactions-tab">
-            <h1>Transaction Contents</h1>
+    {{-- Sidebar Content --}}
+    <div class="col-xl-2 col-lg-3 d-md-none d-lg-block">
+        <h4 class="">Account Receivable</h4>
+        {{-- Account Receivable Active --}}
+        <div class="mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Birr 40,000</div>
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                2 Active</div>
+                        </div>
+                        <div class="col-auto">
+                            {{-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        {{-- Proforma Contents --}}
-        <div class="tab-pane fade" id="proforma" role="tabpanel" aria-labelledby="proforma-tab">
-            <h1>Proforma Contents</h1>
+
+        {{-- Account Receivable Overdue --}}
+        <div class="mb-4">
+            <div class="card border-left-danger shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Birr 215,000</div>
+                            <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                5 Over Due</div>
+                        </div>
+                        <div class="col-auto">
+                            {{-- <i class="fas fa-dollar-sign fa-2x text-gray-300"></i> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+
 </div>
+
+
 
 {{-- Modals --}}
 {{-- New Receipt --}}
