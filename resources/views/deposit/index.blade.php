428c124e94157df97e45d5eee688d244e43000a1
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-02-03 20:47:04 +0800

    Transfer New Deposit Form from Page to Modal

--- resources/views/deposit/index.blade.php
+++ resources/views/deposit/index.blade.php
@@ -17,12 +17,12 @@
 @section('content')
 {{-- Button Group Navigation --}}
 <div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
-    <a role="button" class="btn btn-primary" href="{{ route('deposit.new') }}">
+    <button role="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-deposit">
         <span class="icon text-white-50">
             <i class="fas fa-pen"></i>
         </span>
         <span class="text">New</span>
-    </a> 
+    </button> 
 </div>
 
 {{-- Page Content --}}
@@ -59,21 +59,67 @@
 
 {{-- Modals --}}
 {{-- New Receipt --}}
-<div class="modal fade" id="modal-new-deposit" tabindex="-1" role="dialog" aria-labelledby="modal-new-deposit-label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+<div class="modal fade" id="modal-deposit" tabindex="-1" role="dialog" aria-labelledby="modal-deposit-label" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="modal-new-deposit-label">New Deposit</h5>
+                <h5 class="modal-title" id="modal-deposit-label">New Deposit</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                ...
+                <form id="form-new-deposit" method="post" enctype="multipart/form-data">
+                    <div class="form-group row">
+                        <label for="bank_account" class="col-sm-3 col-form-label">Select Bank<span class="text-danger ml-1">*</span></label>
+                        <div class="col-sm-9">
+                            <select class="form-control" id="bank_account" name="bank_account">
+                                <option>Bank A</option>
+                            </select>
+                        </div>
+                    </div>
+                    <hr>
+                    <h2>Undeposited Sales</h2>
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <th id="thead-actions">Actions</th>
+                                <th>Invoice Number</th>
+                                <th>Customer Name</th>
+                                <th>Date</th>
+                                <th>Total</th>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="table-item-content">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="invoices_1483681825" name="invoices[]" value="">
+                                        </div>
+                                    </td>
+                                    <td class="table-item-content"><label for="invoices_1483681825">1483681825</label></td>
+                                    <td class="table-item-content">PocketDevs</td>
+                                    <td class="table-item-content">01/31/2022</td>
+                                    <td class="table-item-content">Birr 1,000</td>
+                                </tr>
+                                <tr>
+                                    <td class="table-item-content">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="invoices_1483681826" name="invoices[]" value="">
+                                        </div>
+                                    </td>
+                                    <td class="table-item-content"><label for="invoices_1483681826">1483681826</label></td>
+                                    <td class="table-item-content">Fullstack HQ</td>
+                                    <td class="table-item-content">02/01/2022</td>
+                                    <td class="table-item-content">Birr 2,000</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-primary">Save Deposit</button>
             </div>
         </div>
     </div>
