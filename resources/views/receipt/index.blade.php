1e77b77843ba0185ff4808b69923446eea0afeae
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-02-03 21:26:16 +0800

    Added Import and Export Modals for Customer Menu

--- resources/views/receipt/index.blade.php
+++ resources/views/receipt/index.blade.php
@@ -45,13 +45,13 @@
                     <a role="button" class="dropdown-item" data-toggle="modal" data-target="#modal-proforma">Proforma</a>
                 </div>
             </div>
-            <button type="button" class="btn btn-secondary">
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-import">
                 <span class="icon text-white-50">
                     <i class="fas fa-file-import"></i>
                 </span>
                 <span class="text">Import</span>
             </button>
-            <button type="button" class="btn btn-secondary">
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-export">
                 <span class="icon text-white-50">
                     <i class="fas fa-download"></i>
                 </span>
@@ -278,4 +278,65 @@
         </div>
     </div>
 </div>
+
+{{-- Import --}}
+<div class="modal fade" id="modal-import" tabindex="-1" role="dialog" aria-labelledby="modal-import-label" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-import-label">Import Receipts</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="form-import" method="post" enctype="multipart/form-data">
+                    <div class="form-group row">
+                        <label for="revenue_type" class="col-sm-4 col-form-label">File<span class="text-danger ml-1">*</span></label>
+                        <div class="col-sm-8">
+                            <input type="file" id="file" name="file" class="mt-1" required>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" form="form-import">Import Receipts</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+{{-- Export --}}
+<div class="modal fade" id="modal-export" tabindex="-1" role="dialog" aria-labelledby="modal-export-label" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-export-label">Export Receipts</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form id="form-export" method="post" enctype="multipart/form-data">
+                    <div class="form-group row">
+                        <label for="file_type" class="col-sm-4 col-form-label">File Type<span class="text-danger ml-1">*</span></label>
+                        <div class="col-sm-8">
+                            <select class="form-control" id="file_type" name="file_type" required>
+                                <option>HTML</option>
+                                <option>PDF</option>
+                                <option>CSV</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" form="form-export">Export Receipts</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
