b15e7c713880c4195baa414cc62cd60d7ec24e49
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-02-05 15:17:24 +0800

    Move nested modal script hack to global scripts template

--- resources/views/receipt/index.blade.php
+++ resources/views/receipt/index.blade.php
@@ -396,13 +396,3 @@
 </div>
 
 @endsection
-
-@push('scripts')
-<script>
-    $(document).on('show.bs.modal', '.modal', function() {
-    const zIndex = 1040 + 10 * $('.modal:visible').length;
-    $(this).css('z-index', zIndex);
-    setTimeout(() => $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack'));
-    });
-</script>
-@endpush
--- resources/views/template/script.blade.php
+++ resources/views/template/script.blade.php
@@ -8,6 +8,15 @@
     <!-- Custom scripts for all pages-->
     <script src="{{URL::asset('js/sb-admin-2.min.js')}}"></script> 
 
+    {{-- The following script allows nested modals. --}}
+    <script>
+        $(document).on('show.bs.modal', '.modal', function() {
+            const zIndex = 1040 + 10 * $('.modal:visible').length;
+            $(this).css('z-index', zIndex);
+            setTimeout(() => $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack'));
+        });
+    </script>
+
        <!-- Dump all dynamic scripts into template -->
         @stack('scripts')
 
