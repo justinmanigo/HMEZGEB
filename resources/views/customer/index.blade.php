046bc358ce5e2d343b06d2d3a4b86e8dd3b81380
Graeme Xyber Pastoril <xyber.pastoril@protonmail.com>
2022-02-05 15:40:27 +0800

    Update `New Customer` Modal
    * Increased width
    * Divided similar fields into columns
    * Moved checkbox (active) near the submit button

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -133,7 +133,7 @@
 
 {{-- Customer Modal --}}
 <div class="modal fade" id="modal-customer" tabindex="-1" role="dialog" aria-labelledby="modal-customer-label" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="modal-customer-label">New Customer</h5>
@@ -144,90 +144,86 @@
             <div class="modal-body">
                 <form id="form-customer" method="post" enctype="multipart/form-data">
                     <div class="form-group row">
-                        <label for="c_name" class="col-sm-3 col-form-label">Customer Name<span class="text-danger ml-1">*</span></label>
-                        <div class="col-sm-9">
+                        <label for="c_name" class="col-sm-3 col-lg-2 col-form-label">Name<span class="text-danger ml-1">*</span></label>
+                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
                             <input type="text" class="form-control" id="c_name" name="name" placeholder="" required>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_tin_number" class="col-sm-3 col-form-label">Tin Number</label>
-                        <div class="col-sm-9">
+
+                        <label for="c_tin_number" class="col-sm-3 col-lg-2 col-form-label">Tin Number</label>
+                        <div class="col-sm-9 col-lg-4">
                             <input type="text" class="form-control" id="c_tin_number" name="tin_number" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_address" class="col-sm-3 col-form-label">Address</label>
-                        <div class="col-sm-9">
+                        <label for="c_address" class="col-sm-3 col-lg-2 col-form-label">Address</label>
+                        <div class="col-sm-9 col-lg-10">
                             <input type="text" class="form-control" id="c_address" name="address" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_city" class="col-sm-3 col-form-label">City</label>
-                        <div class="col-sm-9">
+                        <label for="c_city" class="col-sm-3 col-lg-2 col-form-label">City</label>
+                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
                             <input type="text" class="form-control" id="c_city" name="city" placeholder="">
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_country" class="col-sm-3 col-form-label">Country</label>
-                        <div class="col-sm-9">
+
+                        <label for="c_country" class="col-sm-3 col-lg-2 col-form-label">Country</label>
+                        <div class="col-sm-9 col-lg-4">
                             <input type="text" class="form-control" id="c_country" name="country" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_phone_1" class="col-sm-3 col-form-label">Phone 1</label>
-                        <div class="col-sm-9">
+                        <label for="c_phone_1" class="col-sm-3 col-lg-2 col-form-label">Phone 1</label>
+                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
                             <input type="text" class="form-control" id="c_phone_1" name="phone_1" placeholder="">
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_phone_2" class="col-sm-3 col-form-label">Phone 2</label>
-                        <div class="col-sm-9">
+
+                        <label for="c_phone_2" class="col-sm-3 col-lg-2 col-form-label">Phone 2</label>
+                        <div class="col-sm-9 col-lg-4">
                             <input type="text" class="form-control" id="c_phone_2" name="phone_2" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_fax" class="col-sm-3 col-form-label">Fax</label>
-                        <div class="col-sm-9">
+                        <label for="c_fax" class="col-sm-3 col-lg-2 col-form-label">Fax</label>
+                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
                             <input type="text" class="form-control" id="c_fax" name="fax" placeholder="">
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_mobile_number" class="col-sm-3 col-form-label">Mobile Number</label>
-                        <div class="col-sm-9">
+
+                        <label for="c_mobile_number" class="col-sm-3 col-lg-2 col-form-label">Mobile Number</label>
+                        <div class="col-sm-9 col-lg-4">
                             <input type="text" class="form-control" id="c_mobile_number" name="mobile_number" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_contact_person" class="col-sm-3 col-form-label">Contact Person</label>
-                        <div class="col-sm-9">
+                        <label for="c_contact_person" class="col-sm-2 col-form-label">Contact Person</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" id="c_contact_person" name="contact_person" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_email" class="col-sm-3 col-form-label">E-mail</label>
-                        <div class="col-sm-9">
+                        <label for="c_email" class="col-sm-2 col-form-label">E-mail</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" id="c_email" name="email" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_website" class="col-sm-3 col-form-label">Website</label>
-                        <div class="col-sm-9">
+                        <label for="c_website" class="col-sm-2 col-form-label">Website</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" id="c_website" name="website" placeholder="">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="c_picture" class="col-sm-3 col-form-label">Picture</label>
-                        <div class="col-sm-9">
+                        <label for="c_picture" class="col-sm-2 col-form-label">Picture</label>
+                        <div class="col-sm-10">
                             <input type="file" id="c_picture" name="picture">
                         </div>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" id="c_is_active" type="checkbox" value="" name="is_active">
-                        <label class="form-check-label" for="c_is_active">Mark Customer as Active</label>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
+                <div class="form-check mr-3">
+                    <input class="form-check-input" id="c_is_active" type="checkbox" value="" name="is_active">
+                    <label class="form-check-label" for="c_is_active">Mark Customer as Active</label>
+                </div>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary" form="modal-customer">Save Customer</button>
             </div>
