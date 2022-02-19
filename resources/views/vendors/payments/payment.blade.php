2ddf839fb634928a0eefe1134be97ca587792fea
Lester Niel <lesterfonggi123@gmail.com>
2022-02-19 15:31:16 +0800

    payment datatables

--- resources/views/vendors/payments/payment.blade.php
+++ resources/views/vendors/payments/payment.blade.php
@@ -17,7 +17,7 @@
         <div class="card-body tab-content">
             <!--Bill Payment content--->
             <div class="table-responsive tab-pane fade show active bill_payment">
-                <table class="table table-bordered" width="100%" cellspacing="0">
+                <table class="table table-bordered" id="dataTables" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>Vendor Name</th>
@@ -68,46 +68,56 @@
             </div>
             <!--Other Payment content--->
             <div class="table-responsive tab-pane fade other_payment">
-                <table class="table table-bordered" width="100%" cellspacing="0">
-                    <thead>
-                        <tr>
-                            <th>Vendor Name</th>
-                            <th>Date</th>
-                            <th>Due Date</th>
-                            <th>Bill No.</th>
-                            <th>Bill Type</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr onclick="window.location='/individualBill'">
-                            <td>Anna Halcon</td>
-                            <td>December 20, 2021</td>
-                            <td>February 14, 2022</td>
-                            <td>000293</td>
-                            <td>Credit Invoice</td>
-                            <td>Paid</td>
-                        </tr>
-                        <tr>
-                            <td>Alvin Dexter</td>
-                            <td>December 22, 2021</td>
-                            <td>February 10, 2022</td>
-                            <td>000293</td>
-                            <td>Credit Invoice</td>
-                            <td>Partially Paid</td>
-                        </tr>
-                        <tr>
-                            <td>Ate Che-Che</td>
-                            <td>January 11, 2021</td>
-                            <td>February 15, 2022</td>
-                            <td>000255</td>
-                            <td>Credit Invoice</td>
-                            <td>Paid</td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTables2" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th>Vendor Name</th>
+                                <th>Date</th>
+                                <th>Due Date</th>
+                                <th>Bill No.</th>
+                                <th>Bill Type</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr onclick="window.location='/individualBill'">
+                                <td>Anna Halcon</td>
+                                <td>December 20, 2021</td>
+                                <td>February 14, 2022</td>
+                                <td>000293</td>
+                                <td>Credit Invoice</td>
+                                <td>Paid</td>
+                            </tr>
+                            <tr>
+                                <td>Alvin Dexter</td>
+                                <td>December 22, 2021</td>
+                                <td>February 10, 2022</td>
+                                <td>000293</td>
+                                <td>Credit Invoice</td>
+                                <td>Partially Paid</td>
+                            </tr>
+                            <tr>
+                                <td>Ate Che-Che</td>
+                                <td>January 11, 2021</td>
+                                <td>February 15, 2022</td>
+                                <td>000255</td>
+                                <td>Credit Invoice</td>
+                                <td>Paid</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function () {
+        $('#dataTables').DataTable();
+        $('#dataTables2').DataTable();
+        $('.dataTables_filter').addClass('pull-right');
+    });
+</script>
 @endsection
