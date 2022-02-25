949666156fd87b2560c18031a8c2974b41e0e2bc
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-02-25 19:47:17 +0800

    Removed `Super Admin` definition
    After reviewing docs, super admins can have specific privileges.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,11 +24,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        // Implicitly grant "Super Admin" role all permissions
-        // This works in the app by using gate-related functions like auth()->user->can() and @can()
-        Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super Admin') ? true : null;
-        });
     }
 }
