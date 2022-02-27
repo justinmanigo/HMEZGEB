30d999c19432add0736a985d8f7f50b214e9e487
Lester Niel <lesterfonggi123@gmail.com>
2022-02-27 12:47:34 +0800

    Modal UI some changes

--- resources/views/vendors/bills/forms/addBillModal.blade.php
+++ resources/views/vendors/bills/forms/addBillModal.blade.php
@@ -1,74 +1,54 @@
 <div class="modal-body h6">	
     <div class="row form-group">	
-        <div class="col-lg-6">
-            <label for="cars">Bill Status:</label>
+            <label for="cars" class="col-form-label col-lg-2">Bill Status:</label>
 
-            <select id="cars" class="form-control">
+            <select id="cars" class="form-control col-lg-4">
             <option value="#" selected disabled hidden>Choose</option>
             <option value="#">Paid Bill</option>
             <option value="#">Partially Paid Bill</option>
             <option value="#">Unpaid Bill</option>
             </select>
-        </div>	
-        <div class="col-lg-6">
-            <label for="name">Vendor name:</label>
-            <input type="text" name="name" required class="form-control">
-        </div>
+            <label for="name" class="col-form-label col-lg-2">Vendor name:</label>
+            <input type="text" name="name" required class="form-control col-lg-4">
     </div>	
     <div class="row form-group">
-        <div class="col">
-            <label for="email">Date:</label>
-            <input type="date" name="address" class="form-control">
-        </div>
-        <div class="col">
-            <label for="email">Due date:</label>
-            <input type="date" name="city" class="form-control">
-        </div>
+            <label for="email" class="col-form-label col-lg-2">Date:</label>
+            <input type="date" name="address" class="form-control col-lg-4">
+            <label for="email" class="col-form-label col-lg-2">Due date:</label>
+            <input type="date" name="city" class="form-control col-lg-4">
     </div>
     <div class="row form-group">
-        <div class="col">
-            <label for="email">Bill number:</label>
-            <input type="number" name="country" class="form-control">
-        </div>
-        <div class="col">
-            <label for="email">Order number:</label>
-            <input type="number" name="tinNum" class="form-control">
-        </div>
+            <label for="email" class="col-form-label col-lg-2">Bill number:</label>
+            <input type="number" name="country" class="form-control col-lg-4">
+            <label for="email" class="col-form-label col-lg-2">Order number:</label>
+            <input type="number" name="tinNum" class="form-control col-lg-4">
     </div>
     <div class="form-group row">
-        <div class="col-lg-6">
-            <label for="email">Item table:</label>
-            <input type="text" name="fax" class="form-control">
-        </div>
-        <div class="col-lg-6">
-            <label for="cars">Cash from:</label>
+            <label for="email" class="col-form-label col-lg-2">Item table:</label>
+            <input type="text" name="fax" class="form-control col-lg-4">
+            <label for="cars" class="col-form-label col-2">Cash from:</label>
     
-            <select id="cars" class="form-control">
+            <select id="cars" class="form-control col-lg-4">
             <option value="#">Cash on hand</option>
             <option value="#">sample1</option>
             <option value="#">Sample2</option>
             </select>
-        </div>
     </div>
     <div class="row form-group">
-        <div class="col-lg-6">
-            <label for="#">Bill Type:</label>
-            <select id="cars" class="form-control">
+            <label for="#" class="col-form-label col-lg-2">Bill Type:</label>
+            <select id="cars" class="form-control col-lg-4">
                 <option value="#" selected disabled hidden>Choose...</option>
                 <option value="#">Cash Bill</option>
                 <option value="#">Credit Bill</option>
+                <option value="#">Other</option>
                 </select>
-        </div>
-        <div class="col">
-            <label for="email">Attachment:</label>
-            <div class="input-group mb-3">
+            <label for="email" class="col-form-label col-lg-2">Attachment:</label>
+            <div class="input-group col-md-2 col-lg-4">
                 <div class="custom-file">
                 <input type="file" class="custom-file-input" id="inputGroupFile03">
                 <label class="custom-file-label" for="inputGroupFile03">Choose file</label>
                 </div>
             </div>
-        </div>
-        
     </div>
     <div class="form-group">
         <label for="email">Bill note:</label>
---
+++ resources/views/vendors/vendors/forms/addVendorModal.blade.php
@@ -0,0 +1,47 @@
+<div class="modal-body h6">				
+    <div class="form-group row">
+        <label for="name" class="col-md-4 col-lg-2 col-form-label">Vendor name:</label>
+        <input type="text" name="name" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Address:</label>
+        <input type="text" name="address" class="form-control col-lg-4">
+    </div>
+    <div class="row form-group">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">City:</label>
+        <input type="text" name="city" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Country:</label>
+        <input type="text" name="country" class="form-control col-lg-4">
+    </div>
+    <div class="form-group row">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Email:</label>
+        <input type="email" name="email" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">TIN number:</label>
+        <input type="text" name="tinNum" class="form-control col-lg-4">
+    </div>
+    <div class="row form-group">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Phone number 1:</label>
+        <input type="text" name="phone1" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Phone number 2:</label>
+        <input type="text" name="phone1" class="form-control col-lg-4">
+    </div>
+    <div class="row form-group">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">FAX:</label>
+        <input type="text" name="fax" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Mobile number:</label>
+        <input type="text" name="mobile_num" class="form-control col-lg-4">
+    </div>
+    <div class="row form-group">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Contact Person:</label>
+        <input type="text" name="contact_person" class="form-control col-lg-4">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Image:</label>
+        <div class="input-group col-md-2 col-lg-4">
+            <div class="custom-file">
+            <input type="file" class="custom-file-input" id="inputGroupFile03">
+            <label class="custom-file-label" for="inputGroupFile03">Choose file</label>
+            </div>
+        </div>
+    </div>
+    <div class="row form-group">
+        <label for="email" class="col-md-4 col-lg-2 col-form-label">Website:</label>
+        <input type="text" name="website" class="form-control col-md-8 col-lg-10">
+    </div>
+</div>
