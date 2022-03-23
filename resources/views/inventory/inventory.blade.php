1f5bd07cd6546db4ded2fdddda7ef909539f11a6
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-03-24 07:51:42 +0800

    Inventory Item Form Fixes

--- resources/views/inventory/inventory.blade.php
+++ resources/views/inventory/inventory.blade.php
@@ -5,6 +5,12 @@
    .w-15 { 
        width:15%;
     }
+
+    .inputPrice::-webkit-inner-spin-button, .inputTax::-webkit-inner-spin-button,
+    .inputPrice::-webkit-outer-spin-button, .inputTax::-webkit-outer-spin-button {
+        -webkit-appearance: none; 
+        margin: 0; 
+    }
 </style>
 @endpush
 
