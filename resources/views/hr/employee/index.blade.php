846db384d0888f7ff6734c97fffd7179ebf9e254
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-04-02 10:44:01 +0800

    Show success msg on store/update/delete employee

--- resources/views/hr/employee/index.blade.php
+++ resources/views/hr/employee/index.blade.php
@@ -33,6 +33,14 @@
 {{-- Page Content --}}
 <div class="card">
     <div class="card-body">
+        @if(session()->has('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ session()->get('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
         <div class="table-responsive">
             <table class="table table-bordered" id="dataTables" width="100%" cellspacing="0">
                 <thead>
