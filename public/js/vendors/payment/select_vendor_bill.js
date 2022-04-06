ea19657f9979d6d0aeaf3fd32ee9bce21da8113d
Patrick <patrickvelasco31@gmail.com>
2022-04-06 12:37:11 +0800

    implement tagify vendor select bill

---
+++ public/js/vendors/payment/select_vendor_bill.js
@@ -0,0 +1,81 @@
+var bill_select_vendor_elm = document.querySelector('#b_vendor');
+
+// initialize Tagify on the above input node reference
+var bill_select_vendor_tagify = new Tagify(bill_select_vendor_elm, {
+    tagTextProp: 'name', // very important since a custom template is used with this property as text
+    enforceWhitelist: true,
+    mode : "select",
+    skipInvalid: false, // do not remporarily add invalid tags
+    dropdown: {
+        closeOnSelect: true,
+        enabled: 0,
+        classname: 'vendors-list',
+        searchKeys: ['name', 'email']  // very important to set by which keys to search for suggesttions when typing
+    },
+    templates: {
+        tag: vendorTagTemplate,
+        dropdownItem: vendorSuggestionItemTemplate
+    },
+    whitelist: [],
+    // whitelist: [
+    //     {
+    //         "value": 1,
+    //         "name": "Justinian Hattersley",
+    //         "avatar": "https://i.pravatar.cc/80?img=1",
+    //         "email": "jhattersley0@ucsd.edu"
+    //     },
+    //     {
+    //         "value": 2,
+    //         "name": "Antons Esson",
+    //         "avatar": "https://i.pravatar.cc/80?img=2",
+    //         "email": "aesson1@ning.com"
+    //     },
+    // ]
+})
+
+bill_select_vendor_tagify.on('dropdown:show dropdown:updated', onDropdownShow)
+bill_select_vendor_tagify.on('dropdown:select', onBillVendorSelectSuggestion)
+bill_select_vendor_tagify.on('input', onBillVendorInput)
+bill_select_vendor_tagify.on('remove', onBillVendorRemove)
+
+var addAllSuggestionsElm;
+
+function onDropdownShow(e){
+    var dropdownContentElm = e.detail.bill_select_vendor_tagify.DOM.dropdown.content;
+}
+
+function onBillVendorSelectSuggestion(e){
+    // checks for data of selected vendor
+    console.log(e.detail.data);
+
+    $("#b_vendor_id").val(e.detail.data.value)
+    $("#b_address").val(e.detail.data.address)
+    $("#b_contact_person").val(e.detail.data.contact_person)
+    $("#b_telephone_number").val(e.detail.data.telephone_one)
+}
+
+function onBillVendorRemove(e){
+    $("#b_vendor_id").val("")
+    $("#b_address").val("")
+    $("#b_contact_person").val("")
+    $("#b_telephone_number").val("")
+}
+
+function onBillVendorInput(e) {
+    var value = e.detail.value
+    bill_select_vendor_tagify.whitelist = null // reset the whitelist
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/AbortController/abort
+    controller && controller.abort()
+    controller = new AbortController()
+
+    // show loading animation and hide the suggestions dropdown
+    bill_select_vendor_tagify.loading(true).dropdown.hide()
+
+    fetch('/select/search/vendor/' + value, {signal:controller.signal})
+        .then(RES => RES.json())
+        .then(function(newWhitelist){
+            bill_select_vendor_tagify.whitelist = newWhitelist // update whitelist Array in-place
+            bill_select_vendor_tagify.loading(false).dropdown.show(value) // render the suggestions dropdown
+        })
+}
--- resources/views/vendors/bills/forms/addBillModal.blade.php
+++ resources/views/vendors/bills/forms/addBillModal.blade.php
@@ -5,7 +5,7 @@
             <div class="form-group row">
                 <label for="b_vendor" class="col-4 col-form-label text-left">Vendor<span class="text-danger ml-1">*</span> :</label>
                 <input class="col-8 col-lg-7" id="b_vendor" name='vendor'>
-                <input type="hidden" id="b_vendob_id" name="vendob_id" value="">
+                <input type="hidden" id="b_vendor_id" name="vendor_id" value="">
             </div>
             {{-- Contact Details --}}
             <div class="form-group row mb-0">
@@ -36,9 +36,9 @@
                 </div>
             </div>
             <div class="form-group row">
-                <label for="b_ordeb_number" class="col-4 col-form-label text-lg-right">Order #<span class="text-danger ml-1">*</span> :</label>
+                <label for="b_order_number" class="col-4 col-form-label text-lg-right">Order #<span class="text-danger ml-1">*</span> :</label>
                 <div class="col-8">
-                    <input type="text" class="form-control" id="b_ordeb_number" name="ordeb_number" placeholder="" required>
+                    <input type="text" class="form-control" id="b_order_number" name="order_number" placeholder="" required>
                 </div>
             </div>
             <div class="form-group row">
