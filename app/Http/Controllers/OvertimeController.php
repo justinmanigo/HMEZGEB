30547842b563daceeefaf64d96a11f31db2db679
Patrick <patrickvelasco31@gmail.com>
2022-04-12 15:36:57 +0800

    added return view in controllers

--- app/Http/Controllers/OvertimeController.php
+++ app/Http/Controllers/OvertimeController.php
@@ -14,7 +14,8 @@ class OvertimeController extends Controller
      */
     public function index()
     {
-        //
+                //
+        return view('hr.overtime.index');
     }
 
     /**
--- app/Http/Controllers/PayrollController.php
+++ app/Http/Controllers/PayrollController.php
@@ -15,6 +15,7 @@ class PayrollController extends Controller
     public function index()
     {
         //
+        return view('hr.payroll.index');
     }
 
     /**
