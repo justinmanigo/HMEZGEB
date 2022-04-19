35eec48278866679debf3c5a6d82b319b3531e2e
Patrick <patrickvelasco31@gmail.com>
2022-04-19 11:03:16 +0800

    added store function for income tax and overtime

---
+++ app/Models/OvertimePayrollRules.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OvertimePayrollRules extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'working_days',
+        'working_hours',
+        'day_rate',
+        'night_rate',
+        'holiday_weekend_rate',
+    ];
+    
+}
