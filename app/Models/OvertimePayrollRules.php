7bb906a78ed98f06635d5e9fc748b257fa9a6e6c
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-04-28 11:05:29 +0800

    Add `user_id` in `OvertimePayrollRules` Model

--- app/Models/OvertimePayrollRules.php
+++ app/Models/OvertimePayrollRules.php
@@ -10,6 +10,7 @@ class OvertimePayrollRules extends Model
     use HasFactory;
 
     protected $fillable = [
+        'user_id',
         'working_days',
         'working_hours',
         'day_rate',
