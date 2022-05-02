42da267c8ea9bf29253c169796f4a3eb97c94de7
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-02 15:01:06 +0800

    Refactor ajax form submission to be dynamic
    To ensure DRY principle

--- public/js/account/update_info.js
+++ public/js/account/update_info.js
@@ -4,27 +4,41 @@ $("#alert-success button").click(function(){
 });
 
 // When the update username form is submitted.
-$("#form-username").submit(function(e){
+$(".account-update").submit(function(e){
     e.preventDefault();
+    
+    // Initialize and get input elements, submit button, and close button.
+    // error ids must match ff format: err_{form_name}_{input_field}
+    // for submit btns: btn_submit_{form_name}
+    // for close btns: btn_close_{form_name}
+    // both input and error elements must have data-field bearing the input name.
+    var elm_errors = [], btn_submit, btn_close;
 
-    // Link error elements & submit btn to a variable.
-    var error_text_elements = [
-        $("#u_username_error"), 
-        $("#u_username_confirm_error"), 
-        $("#u_username_password_error")
-    ];
-    var btn_submit = $("#u_username_submit_btn");
-    var btn_close = $("#u_username_close_btn");
+    for(i = 0; i < e.target.length; i++)
+    {
+        let elm_data = e.target[i].dataset;
+        if(elm_data.field != undefined) {
+            console.log(elm_data.field);
+            console.log(`#err_${e.target.id}_${elm_data.field}`);
+            elm_errors.push($(`#err_${e.target.id}_${elm_data.field}`));
+        }
+        if(elm_data.close != undefined) {
+            btn_close = $(`#btn_close_${e.target.id}`)
+        }
+        if(elm_data.submit != undefined) {
+            btn_submit = $(`#btn_submit_${e.target.id}`)
+        }
+    }
 
     // Hide error elements and disable submit btn.
-    hideErrors(error_text_elements);
+    hideErrors(elm_errors);
     disableButton(btn_submit);
 
     // Create request
     var request = $.ajax({
-        url: 'ajax/account/update/username',
+        url: `ajax/account/update/${e.target.dataset.update}`,
         method: "POST",
-        data: $("#form-username").serialize()
+        data: $(`#form_${e.target.dataset.update}`).serialize()
     });
 
     // If request has successfully processed.
@@ -37,7 +51,8 @@ $("#form-username").submit(function(e){
 
     // If request has errors (including validation errors).
     request.fail(function(jqXHR, status, error){
-        showErrors(error_text_elements, jqXHR.responseJSON.errors);
+        console.log(jqXHR);
+        showErrors(elm_errors, jqXHR.responseJSON.errors);
     });
 
     // The following always executes regardless of status.
@@ -53,13 +68,12 @@ function hideErrors(errors_dom)
     });
 }
 
-function showErrors(errors_dom, errors)
+function showErrors(elm_errors, errors)
 {
     error_list = Object.keys(errors);
-
-    errors_dom.forEach(function(e){
-        err = e[0].dataset.error;
-                if(errorInArray(error_list, err)) {
+    elm_errors.forEach(function(e){
+        err = e[0].dataset.field;
+        if(errorInArray(error_list, err)) {
             e.show().html(errors[err][0]);
         }
     });   
@@ -67,15 +81,12 @@ function showErrors(errors_dom, errors)
 
 function errorInArray(error_list, err)
 {
-    console.log("Check if error is in array.");
     var res = false;
     error_list.forEach(function(e){
-        // console.log(`${e} == ${err}? = ${e == err}`);
         if(e == err) {
             res = true;
         }
     });
-    // console.log("Result: " + res);
     return res;
 }
 
