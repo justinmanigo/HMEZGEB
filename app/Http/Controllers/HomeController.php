301d30a228d16941065b9796af6f3a8564ca6033
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-14 10:53:05 +0800

    Skip selection if the number of accounting systems the user belongs is only one

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -18,6 +19,17 @@ class HomeController extends Controller
 
     public function viewAccountingSystems()
     {
+        // Retrieves the accounting systems' the authenticated user belongs.
+        $user = User::find(Auth::id());
+        $user->accountingSystemUsers;
+
+        // If the number of accounting systems' is only one, skip ahead to dashboard.
+        if(count($user->accountingSystemUsers)) {
+            $this->request->session()->put('accounting_system_id', $user->accountingSystemUsers[0]->accounting_system_id);
+            return redirect('/');
+        }
+        
+        // Otherwise, let the authenticated user select which accounting system to manage.
         return view('view-accounting-systems');
     }
 
