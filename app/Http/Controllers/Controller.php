1498a477fee783b87c19555cb5891e75e66841bb
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-14 10:23:11 +0800

    Add class constructor that retrieves `$request` in `Controller`
    For use to check `$request->session()` in any app-related controllers.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,20 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(Request $request)
+    {
+        // Gets request to obtain session details.
+        $this->request = $request;
+    }
 }
