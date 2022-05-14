0fc95c07ba73d95c0fd5c4322f7c193d061d4abc
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-14 15:01:33 +0800

    Add logout link on `\switch` page

--- resources/views/view-accounting-systems.blade.php
+++ resources/views/view-accounting-systems.blade.php
@@ -79,10 +79,7 @@
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="{{-- route('register') --}}">Create an Account!</a>
+                                        <a class="small" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
                                     </div>
                                 </div>
                             </div>
@@ -94,6 +91,30 @@
 
         </div>
 
+    </div>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="{{ route('logout') }}" 
+                     onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
 
         <!-- Bootstrap core JavaScript-->
