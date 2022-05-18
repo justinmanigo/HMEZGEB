475a432aff6b783bb7708a960d4aab4ca65adbc5
Patrick <patrickvelasco31@gmail.com>
2022-05-18 14:15:40 +0800

    setup views for PDF

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -8,119 +8,133 @@ use PDF;
 
 class ReportsController extends Controller
 {
-    // function for customers view
+    // Nav View controller
     public function customers()
     {       
         return view('reports.customers.index');
     }
 
-    // function for vendors view
     public function vendors()
     {
         return view('reports.vendors.index');
     }
 
-    // function for sales
     public function sales()
     {
         return view('reports.sales.index');
     }
 
-    // function for entries
     public function entries()
     {
         return view('reports.entries.index');
     }
 
-    // function for financial statement
     public function financial_statement()
     {
         return view('reports.financial_statement.index');
     }
 
     // PDF
+    // Customers
     public function agedReceivablePDF(Request $request)
     {
         $pdf = \PDF::loadView('reports.customers.pdf.aged_receivable', compact('request'));
         return $pdf->download('aged_receivable.pdf');
     }
     
-
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function cashReceiptsPDF(Request $request)
     {
-        //
-        return view('reports.index');
+        $pdf = \PDF::loadView('reports.vendors.pdf.cash_receipts', compact('request'));
+        return $pdf->download('cash_receipts.pdf');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function customersLedgers(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.sales.pdf.customers_ledgers', compact('request'));
+        return $pdf->download('customers_ledgers.pdf');
+    }
+    
+    // Vendors
+    public function agedPayablePDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.vendors.pdf.aged_payable', compact('request'));
+        return $pdf->download('aged_payable.pdf');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function cashDisbursementsPDF(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.vendors.pdf.cash_disbursements', compact('request'));
+        return $pdf->download('cash_disbursements.pdf');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Reports  $reports
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Reports $reports)
+    public function cashRequirementsPDF(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.sales.pdf.cash_requirements', compact('request'));
+        return $pdf->download('cash_requirements.pdf');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Reports  $reports
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Reports $reports)
+    public function vendorsLedgersPDF(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.sales.pdf.vendors_ledgers', compact('request'));
+        return $pdf->download('vendors_ledgers.pdf');
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Reports  $reports
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Reports $reports)
+    // Sales
+    public function salesPDF(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.sales.pdf.sales', compact('request'));
+        return $pdf->download('sales.pdf');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Reports  $reports
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Reports $reports)
+    // Entries
+    public function generalLedgerPDF(Request $request)
     {
-        //
+        $pdf = \PDF::loadView('reports.entries.pdf.general_ledger', compact('request'));
+        return $pdf->download('general_ledger.pdf');
     }
+
+    public function generalJournalPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.entries.pdf.general_journal', compact('request'));
+        return $pdf->download('general_journal.pdf');
+    }
+
+    public function receiptPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.entries.pdf.receipt', compact('request'));
+        return $pdf->download('receipt.pdf');
+    }
+
+    public function billPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.entries.pdf.bill', compact('request'));
+        return $pdf->download('bill.pdf');
+    }
+    
+    public function paymentPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.entries.pdf.payment', compact('request'));
+        return $pdf->download('payment.pdf');
+    }
+
+    public function journalVoucherPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.entries.pdf.journal_voucher', compact('request'));
+        return $pdf->download('journal_voucher.pdf');
+    }
+    
+    // Financial Statement
+    public function balanceSheetPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.financial_statement.pdf.balance_sheet', compact('request'));
+        return $pdf->download('balance_sheet.pdf');
+    }
+    
+    public function incomeStatementPDF(Request $request)
+    {
+        $pdf = \PDF::loadView('reports.financial_statement.pdf.income_statement', compact('request'));
+        return $pdf->download('income_statement.pdf');
+    }
+
+  
 }
