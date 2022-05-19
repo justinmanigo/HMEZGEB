7f3c41e4732ee90ab1b8196b7fb99c647bbbcb71
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-19 09:16:14 +0800

    Set default values of certain fields in `Subscription` Model

--- database/migrations/2022_02_04_013739_create_subscriptions_table.php
+++ database/migrations/2022_02_04_013739_create_subscriptions_table.php
@@ -16,18 +16,18 @@ class CreateSubscriptionsTable extends Migration
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained();
-            $table->tinyInteger('account_limit');
-            $table->date('trial_from')->nullable();
-            $table->date('trial_to')->nullable();
+            $table->tinyInteger('account_limit')->default(3);
             $table->enum('account_type', [      // this part is something to be clarified
                 'admin',                        // later on in the progress meeting with
                 'moderator',                    // the client.
                 'member',
             ])->default('admin');
+            $table->date('trial_from')->nullable()->default(now()->format('Y-m-d'));
+            $table->date('trial_to')->nullable()->default(now()->addWeek()->format('Y-m-d'));
             $table->enum('payment_status', [ 
                 'pending',
                 'paid',
-            ])->nullable();
+            ])->nullable()->default('pending');
             $table->timestamp('active_since_at')->nullable();
             $table->timestamps();
         });
