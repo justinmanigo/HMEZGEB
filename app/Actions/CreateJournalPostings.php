0b10a0b7f272704a0a471fb8ae6537580bc43858
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-28 16:38:56 +0800

    Update `CreateJournalPostings` Action to process processed tagify fields

--- app/Actions/CreateJournalPostings.php
+++ app/Actions/CreateJournalPostings.php
@@ -2,44 +2,42 @@
 
 namespace App\Actions;
 
-use Lorisleiva\Actions\Concerns\AsAction;
+use App\Actions\DecodeTagifyField;
 use App\Models\Settings\ChartOfAccounts\ChartOfAccounts;
 use App\Models\Settings\ChartOfAccounts\JournalPostings;
+use Lorisleiva\Actions\Concerns\AsAction;
 
 class CreateJournalPostings
 {
     use AsAction;
 
-    public function handle($journal_entry, $raw_debit_accts, $debit_amount, $raw_credit_accts, $credit_amount)
+    public function handle($journal_entry, $debit_accounts, $debit_amount, $credit_accounts, $credit_amount, $accounting_system_id)
     {
-        for($i = 0; $i < count($raw_debit_accts); $i++)
+        for($i = 0; $i < count($debit_accounts); $i++)
         {
-            $debit_accounts[$i] = $this->decodeRawAccount($raw_debit_accts[$i]);
+            // $debit_accounts[$i] = $this->decodeRawAccount($raw_debit_accts[$i]);
             $coa = ChartOfAccounts::find($debit_accounts[$i]->value);
 
-            $updated_balance = $this->computeCOAUpdatedBalance('debit', $debit_accounts[$i], $coa, $debit_amount[$i]);
-            $this->updateCOABalance($coa, $updated_balance);
-            $this->createJournalPosting('debit', $journal_entry->id, $debit_accounts[$i]->value, $credit_amount[$i]);
+            // $updated_balance = $this->computeCOAUpdatedBalance('debit', $debit_accounts[$i], $coa, $debit_amount[$i]);
+            // $this->updateCOABalance($coa, $updated_balance);
+            $this->createJournalPosting('debit', $journal_entry->id, $debit_accounts[$i]->value, $debit_amount[$i], $accounting_system_id);
         }
 
         // Credits
-        for($i = 0; $i < count($raw_credit_accts); $i++)
+        for($i = 0; $i < count($credit_accounts); $i++)
         {
-            $credit_accounts[$i] = $this->decodeRawAccount($raw_credit_accts[$i]);
+            // $credit_accounts[$i] = $this->decodeRawAccount($raw_credit_accts[$i]);
             $coa = ChartOfAccounts::find($credit_accounts[$i]->value);
 
-            $updated_balance = $this->computeCOAUpdatedBalance('credit', $credit_accounts[$i], $coa, $credit_amount[$i]);
-            $this->updateCOABalance($coa, $updated_balance);
-            $this->createJournalPosting('credit', $journal_entry->id, $credit_accounts[$i]->value, $credit_amount[$i]);
+            // $updated_balance = $this->computeCOAUpdatedBalance('credit', $credit_accounts[$i], $coa, $credit_amount[$i]);
+            // $this->updateCOABalance($coa, $updated_balance);
+            $this->createJournalPosting('credit', $journal_entry->id, $credit_accounts[$i]->value, $credit_amount[$i], $accounting_system_id);
         }
     }
 
-    public function decodeRawAccount($acct)
-    {
-        $item = json_decode($acct);
-        return $item[0];
-    }
-
+    /**
+     * TODO: Rework later when needed.
+     */
     public function computeCOAUpdatedBalance($type, $acct, $coa, $amount)
     {
         if($type == 'debit')
@@ -60,6 +58,9 @@ class CreateJournalPostings
         return $updated_balance;
     }
 
+    /**
+     * TODO: Rework later when needed.
+     */
     public function updateCOABalance($coa, $updated_balance)
     {
         $coa->update([
@@ -67,9 +68,10 @@ class CreateJournalPostings
         ]);
     }
 
-    public function createJournalPosting($type, $id, $coa_id, $amount)
+    public function createJournalPosting($type, $id, $coa_id, $amount, $accounting_system_id)
     {
         JournalPostings::create([
+            'accounting_system_id' => $accounting_system_id,
             'type' => $type,
             'journal_entry_id' => $id,
             'chart_of_account_id' => $coa_id,
