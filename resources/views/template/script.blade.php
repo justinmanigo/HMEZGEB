500929e0155562800f199e8acf6e7d490fa6e015
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-28 16:33:58 +0800

    Added `ajax-submit-updated` to global scripts

--- resources/views/template/script.blade.php
+++ resources/views/template/script.blade.php
@@ -35,6 +35,7 @@
 
     <script src="{{ url('/js/toast.js') }}"></script>
     <script src="{{ url('/js/form-submit-ajax.js') }}"></script>
+    <script src="{{ url('/js/ajax-submit-updated.js') }}"></script>
 
        <!-- Dump all dynamic scripts into template -->
         @stack('scripts')
