8ffa2691a0849a7ceb8301e6f9b8a91d3f4053bf
Graeme Xyber Pastoril <xyber.pastoril@gmail.com>
2022-05-28 15:48:57 +0800

    Remove unnecessary commented code on jv form

--- resources/views/journals/index.blade.php
+++ resources/views/journals/index.blade.php
@@ -263,76 +263,9 @@
                         <label for="jv_notes" class="col-form-label">Notes:</label>
                         <textarea class="form-control" id="jv_notes" name="notes"></textarea>
                     </div>
-                    {{-- <div class="form-group row">
-                        <label for="c_address" class="col-sm-3 col-lg-2 col-form-label">Address</label>
-                        <div class="col-sm-9 col-lg-10">
-                            <input type="text" class="form-control" id="c_address" name="address" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_city" class="col-sm-3 col-lg-2 col-form-label">City</label>
-                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
-                            <input type="text" class="form-control" id="c_city" name="city" placeholder="">
-                        </div>
-
-                        <label for="c_country" class="col-sm-3 col-lg-2 col-form-label">Country</label>
-                        <div class="col-sm-9 col-lg-4">
-                            <input type="text" class="form-control" id="c_country" name="country" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_phone_1" class="col-sm-3 col-lg-2 col-form-label">Phone 1</label>
-                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
-                            <input type="text" class="form-control" id="c_phone_1" name="phone_1" placeholder="">
-                        </div>
-
-                        <label for="c_phone_2" class="col-sm-3 col-lg-2 col-form-label">Phone 2</label>
-                        <div class="col-sm-9 col-lg-4">
-                            <input type="text" class="form-control" id="c_phone_2" name="phone_2" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_fax" class="col-sm-3 col-lg-2 col-form-label">Fax</label>
-                        <div class="col-sm-9 col-lg-4 mb-3 mb-lg-0">
-                            <input type="text" class="form-control" id="c_fax" name="fax" placeholder="">
-                        </div>
-
-                        <label for="c_mobile_number" class="col-sm-3 col-lg-2 col-form-label">Mobile Number</label>
-                        <div class="col-sm-9 col-lg-4">
-                            <input type="text" class="form-control" id="c_mobile_number" name="mobile_number" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_contact_person" class="col-sm-2 col-form-label">Contact Person</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="c_contact_person" name="contact_person" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_email" class="col-sm-2 col-form-label">E-mail</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="c_email" name="email" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_website" class="col-sm-2 col-form-label">Website</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="c_website" name="website" placeholder="">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label for="c_picture" class="col-sm-2 col-form-label">Picture</label>
-                        <div class="col-sm-10">
-                            <input type="file" id="c_picture" name="picture">
-                        </div>
-                    </div> --}}
                 </form>
             </div>
             <div class="modal-footer">
-                <div class="form-check mr-3">
-                    {{-- <input class="form-check-input" id="c_is_active" type="checkbox" value="" name="is_active">
-                    <label class="form-check-label" for="c_is_active">Mark Customer as Active</label> --}}
-                </div>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                 <button type="submit" class="btn btn-primary" id="form-jv-save-btn" form="form-jv">Save Journal Voucher</button>
             </div>
