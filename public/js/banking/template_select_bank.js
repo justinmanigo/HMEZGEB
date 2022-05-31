be2b40a241de736c83bf3198ea35f1149b00598f
Patrick <patrickvelasco31@gmail.com>
2022-05-31 17:41:40 +0800

    setup view and tagify for bank select

---
+++ public/js/banking/template_select_bank.js
@@ -0,0 +1,35 @@
+function bankTagTemplate(tagData){
+    return `
+        <tag title="${tagData.bank_account_number}"
+                contenteditable='false'
+                spellcheck='false'
+                tabIndex="-1"
+                class="tagify__tag ${tagData.class ? tagData.class : ""}"
+                ${this.getAttributes(tagData)}>
+            <x title='' class='tagify__tag__removeBtn' role='button' aria-label='remove tag'></x>
+            <div>
+                <div class='tagify__tag__avatar-wrap'>
+                    <img onerror="this.style.visibility='hidden'" src="${tagData.avatar}">
+                </div>
+                <span class='tagify__tag-text'>${tagData.bank_branch}</span>
+            </div>
+        </tag>
+    `
+}
+
+function bankSuggestionItemTemplate(tagData){
+    return `
+        <div ${this.getAttributes(tagData)}
+            class='tagify__dropdown__item ${tagData.class ? tagData.class : ""}'
+            tabindex="0"
+            role="option">
+            ${ tagData.avatar ? `
+            <div class='tagify__dropdown__item__avatar-wrap'>
+                <img onerror="this.style.visibility='hidden'" src="${tagData.avatar}">
+            </div>` : ''
+            }
+            <strong>${tagData.bank_branch}</strong>
+            <span>${tagData.bank_account_number}</span>
+        </div>
+    `
+}
