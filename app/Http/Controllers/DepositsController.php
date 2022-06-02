d6f97985e4ec3ffd0e051a1cb2c31dff330cfc56
Patrick <patrickvelasco31@gmail.com>
2022-06-02 17:01:18 +0800

    CRUD

--- app/Http/Controllers/DepositsController.php
+++ app/Http/Controllers/DepositsController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Deposits;
+use App\Models\DepositItems;
+use App\Models\Settings\ChartOfAccounts\ChartOfAccounts;
+use App\Models\ReceiptReferences;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
 class DepositsController extends Controller
@@ -14,7 +18,8 @@ class DepositsController extends Controller
      */
     public function index()
     {
-        return view('banking.deposits.index');
+        $deposits = Deposits::all();
+        return view('customer.deposit.index',compact('deposits'));
     }
 
     /**
@@ -35,8 +40,33 @@ class DepositsController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        return $request;
+        $data = json_decode($request->bank_account);
+        $id = $data[0]->value;
+        $coa = ChartOfAccounts::find($id);
+        $deposits = Deposits::create([
+            'chart_of_account_id' => $coa->id,
+            'status' => 'Deposited',
+            'deposit_ticket_date' => $request->deposit_ticket_date,
+            'total_amount' => $request->total_amount,
+            'remark' => $request->remark,
+        ]);
+        for($i = 0; $i < count($request->is_deposited); $i++)
+        {
+            // find the receipt reference using is_deposited value (receipt_reference_id)
+            $receipts = ReceiptReferences::find($request->is_deposited[$i]);
+            // add to coa balance
+            $coa->current_balance += $receipts->receipt->total_amount_received;
+            $coa->save();
+            // update receipt reference is_deposited
+            $receipts->is_deposited = "yes";
+            $receipts->save();
+            // add to deposit items
+            $depositItems = DepositItems::create([
+                'receipt_reference_id' => $request->is_deposited[$i],
+                'deposit_id' => $deposits->id,
+            ]); 
+        }
+        return $coa;
     }
 
     /**
@@ -83,4 +113,32 @@ class DepositsController extends Controller
     {
         //
     }
+
+     /******* AJAX ***********/
+
+     public function ajaxSearchBank($query)
+     {
+         return ChartOfAccounts::select(
+                 'chart_of_accounts.id as value',
+                 'chart_of_accounts.chart_of_account_category_id',
+                 'chart_of_accounts.chart_of_account_no',
+                 'chart_of_accounts.account_name',
+                 'bank_accounts.bank_branch',
+                 'bank_accounts.bank_account_number',
+                 'bank_accounts.bank_account_type',
+                 'chart_of_account_categories.category',
+                 'chart_of_account_categories.normal_balance',
+             )
+             ->leftJoin('chart_of_account_categories', 'chart_of_accounts.chart_of_account_category_id', '=', 'chart_of_account_categories.id')
+             ->leftJoin('bank_accounts', 'chart_of_accounts.id', '=', 'bank_accounts.chart_of_account_id')
+             ->where('chart_of_account_categories.category' , '=', 'Cash')
+             ->where('bank_accounts.bank_account_number' , '!=', NULL)
+             ->where(function($sql) use ($query) {
+                 $sql->where('chart_of_accounts.account_name', 'LIKE', "%{$query}%")
+                     ->orWhere('chart_of_accounts.chart_of_account_no', 'LIKE', "%{$query}%")
+                     ->orWhere('bank_accounts.bank_account_number', 'LIKE', "%{$query}%")
+                     ->orWhere('bank_accounts.bank_branch', 'LIKE', "%{$query}%");
+             })
+             ->get();
+     }
 }
--- app/Http/Controllers/ReceiptController.php
+++ app/Http/Controllers/ReceiptController.php
@@ -366,8 +366,9 @@ class ReceiptController extends Controller
             ->leftJoin('receipts', 'receipts.receipt_reference_id', '=', 'receipt_references.id')
             ->leftJoin('customers', 'customers.id', '=', 'receipt_references.customer_id')
             ->where('receipt_references.status', 'paid')
+            ->where('receipt_references.type', 'receipt')
+            ->where('receipt_references.is_deposited', 'no')
             ->get();
-        Log::info($receipts);
         return $receipts;
     }
 }
