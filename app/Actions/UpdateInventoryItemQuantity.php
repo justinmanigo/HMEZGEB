199f589b0a7e84276df16b2c2dfb355c596bbeb8
Patrick <patrickvelasco31@gmail.com>
2022-06-03 14:41:46 +0800

    Update notification resolved status on Bill create

--- app/Actions/UpdateInventoryItemQuantity.php
+++ app/Actions/UpdateInventoryItemQuantity.php
@@ -4,6 +4,8 @@ namespace App\Actions;
 
 use Lorisleiva\Actions\Concerns\AsAction;
 use App\Models\Inventory;
+use App\Models\Notification;
+use Illuminate\Support\Facades\Log;
 
 class UpdateInventoryItemQuantity
 {
@@ -13,13 +15,45 @@ class UpdateInventoryItemQuantity
     {
         for($i = 0; $i < count($items); $i++)
         {
-            $inv = Inventory::where('id', $items[$i]->value);
+            $inv = Inventory::where('id', $items[$i]->value)->first();
+            // checks for records of unresolved notifs
+            $notif = Notification::where('reference_id', $inv->id)->where('source', 'inventory')->where('resolved',0)->get();
+          
+            if($action == 'increase') 
+            {
+                $inv->increment('quantity', $quantities[$i]);  
 
-            if($action == 'increase') {
-                $inv->increment('quantity', $quantities[$i]);
-            } else {
-                $inv->decrement('quantity', $quantities[$i]);
-            }
+                    // update all warning notifications
+                    if($inv->quantity >= $inv->critical_quantity && $notif->count() > 0)  {
+                        $notif_check = Notification::where('reference_id', $inv->id)->where('source', 'inventory')->where('resolved',0)->get(); 
+                    }                 
+                    // update all danger notifications
+                    else if($inv->quantity > 0 && $notif->count() > 0) {
+                        $notif_check = Notification::where('reference_id', $inv->id)->where('source', 'inventory')->where('type','danger')->where('resolved',0)->get(); 
+                        
+                        // check if there is a created warning notification before reaching danger level 0
+                        $warning_check = Notification::where('reference_id', $inv->id)->where('source', 'inventory')->where('type','warning')->where('resolved',0)->get(); 
+                        if($warning_check->count() == 0) {
+                            // create notification if doesnt reach warning level
+                            Notification::create([
+                                'reference_id' => $inv->id,
+                                'source' => 'inventory',
+                                'message' => 'Inventory item '.$inv->item_name.' is less than or equal to '.$inv->critical_quantity.'. Please reorder.',
+                                'title' => 'Inventory Critical Level',
+                                'type' => 'warning',
+                                'link' => 'vendors/bills',            
+                        ]);                        }
+                    }
+                    
+                    foreach($notif_check as $n)
+                    {
+                        $n->resolved = 1;
+                        $n->time_resolved = now();
+                        $n->save();
+                    }
+                }
+            else if($action == 'decrease')         
+                $inv->decrement('quantity', $quantities[$i]);            
         }
     }
 }
