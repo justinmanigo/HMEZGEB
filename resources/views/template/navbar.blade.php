bbb3a05ac50e87636a032791370d4133173895ec
Xyber Pastoril <xyber.pastoril@proton.me>
2022-06-04 21:27:15 +0800

    Temporarily hide `Settings > Themes` from Navbar

--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -98,7 +98,7 @@
                         <h6 class="collapse-header">Menu:</h6>
                         <a class="collapse-item" href="/settings/company">Company Info</a>
                         <a class="collapse-item" href="/settings/users">Users</a>
-                        <a class="collapse-item" href="/settings/themes">Theme</a>
+                        {{-- <a class="collapse-item" href="/settings/themes">Theme</a> --}}
                         <a class="collapse-item" href="/settings/taxes">Taxes</a>
                         <a class="collapse-item" href="/settings/withholding">Withholding</a>
                         <a class="collapse-item" href="/settings/payroll">Payroll Rules</a>
