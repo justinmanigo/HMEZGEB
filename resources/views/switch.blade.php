457c09318746b215e374826436715bc7fe5e6774
Xyber Pastoril <xyber.pastoril@proton.me>
2022-07-29 18:05:02 +0800

    Add indicator when a user does not have any acct. systems

--- resources/views/switch.blade.php
+++ resources/views/switch.blade.php
@@ -16,29 +16,41 @@
 @section('content')
 
 <div class="container-fluid">
-    <div class="row">
-        <h4 class="mb-4">Choose an Accounting System</h4>
-    </div>
+    
+    @if (count($accountingSystems))
 
-    @foreach($accountingSystems as $account)
-    <div class="row">
-        <div class="card col-12 col-sm-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mb-2">
-            <div class="card-body">
-                <div class="grid-container">
-                    <div>
-                        <h5 class="m-0">{{ $account->name }}</h5>
-                        <small class="m-0">Subscription # {{ $account->subscription_id }} | {{ $account->user_first_name . ' ' . $account->user_last_name }}</small>
-                    </div>
-                    <div>
-                        <button class="btn btn-primary m-0 accounting_system_item" data-id="{{ $account->accounting_system_id }}">
-                            <i class="fas fa-sign-out-alt fa-sm fa-fw text-gray-400"></i>
-                        </button>
+        <div class="row">
+            <h4 class="mb-4">Choose an Accounting System</h4>
+        </div>
+
+        @foreach($accountingSystems as $account)
+        <div class="row">
+            <div class="card col-12 col-sm-12 col-md-8 col-lg-6 col-xl-5 col-xxl-4 mb-2">
+                <div class="card-body">
+                    <div class="grid-container">
+                        <div>
+                            <h5 class="m-0">{{ $account->name }}</h5>
+                            <small class="m-0">Subscription # {{ $account->subscription_id }} | {{ $account->user_first_name . ' ' . $account->user_last_name }}</small>
+                        </div>
+                        <div>
+                            <button class="btn btn-primary m-0 accounting_system_item" data-id="{{ $account->accounting_system_id }}">
+                                <i class="fas fa-sign-out-alt fa-sm fa-fw text-gray-400"></i>
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    @endforeach
+        @endforeach
+    @else
+        <div class="row">
+            <h4 class="mb-4">You are not a member of any accounting systems.</h4>
+        </div>
+        <div class="row">
+            <button class="btn btn-primary m-0" disabled>Sign up for a subscription!</button>
+        </div>
+
+    @endif
 </div>
 
 @endsection
