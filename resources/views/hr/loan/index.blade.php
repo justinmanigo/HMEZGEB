366880cb94915e9db1dd4f4aea6bf0adb7a4c33c
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-08-09 14:24:41 +0800

    Temporarily remove `action` values on certain delete functions of HR

--- resources/views/hr/loan/index.blade.php
+++ resources/views/hr/loan/index.blade.php
@@ -332,7 +332,7 @@
             <div class="modal-body">Are you sure to delete this record?</div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" onClick="dismissModel()">Cancel</button>
-                <form id="delete-frm" class="" action="{{route('loans.loan.destroy', $loan->id)}}" method="POST">
+                <form id="delete-frm" class="" action="" method="POST">
                     @method('DELETE')
                     @csrf
                     <button class="btn btn-danger">Delete</button>
