64a6cf35558f70dc436f80513edebf0414be7066
Patrick <patrickvelasco31@gmail.com>
2022-08-13 15:21:22 +0800

    Make inventory UI consistent

--- resources/views/inventory/index.blade.php
+++ resources/views/inventory/index.blade.php
@@ -2,142 +2,153 @@
 
 @push('styles')
 <style>
-.w-15 {
-    width: 15%;
-}
+    .w-15 {
+        width: 15%;
+    }
 
-.inputPrice::-webkit-inner-spin-button,
-.inputTax::-webkit-inner-spin-button,
-.inputPrice::-webkit-outer-spin-button,
-.inputTax::-webkit-outer-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-}
+    .inputPrice::-webkit-inner-spin-button,
+    .inputTax::-webkit-inner-spin-button,
+    .inputPrice::-webkit-outer-spin-button,
+    .inputTax::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 </style>
 @endpush
 
 @section('content')
 
-<main>
-    <div class="d-flex justify-content-between align-items-start">
-        <!---buttons--->
-        <div class="d-flex">
-            <!--add item btn--->
-            <button type="button" class="btn btn-info mx-1" data-toggle="modal" data-target="#modal-new-item">New
-                Item</button>
-            <div class="modal fade" id="modal-new-item" tabindex="-1" role="dialog"
-                aria-labelledby="modal-newItem-label" aria-hidden="true">
-                <div class="modal-dialog modal-xl" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="modal-receipt-label">New Inventory Item</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        @include('inventory.forms.newItem')
-                    </div>
-                </div>
-            </div>
-
-            <button type="button" class="btn btn-info mx-1" data-toggle="modal"
-                data-target=".bd-example-modal-lg">Import</button>
-            <button type="button" class="btn btn-info mx-1" data-toggle="modal"
-                data-target=".bd-example-modal-lg">Export</button>
-            {{-- <button type="button" class="btn btn-info mx-1" data-toggle="modal"
-                data-target=".bd-example-modal-lg">Download Excel File</button> --}}
+<div class="row">
+    <div class="col-xl-10 col-lg-9 col-12">
+        {{-- Button Group Navigation --}}
+        <div class="btn-group mb-3" role="group" aria-label="Button group with nested dropdown">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-new-item">
+                <span class="icon text-white-50">
+                    <i class="fas fa-pen"></i>
+                </span>
+                <span class="text">New</span>
+            </button>
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-import">
+                <span class="icon text-white-50">
+                    <i class="fas fa-file-import"></i>
+                </span>
+                <span class="text">Import</span>
+            </button>
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modal-export">
+                <span class="icon text-white-50">
+                    <i class="fas fa-download"></i>
+                </span>
+                <span class="text">Export</span>
+            </button>
         </div>
-        <!-- Earnings (Annual) Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-success shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                Total Inventory Value</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $inventoryValue }}</div>
-                        </div>
-                        <div class="col-auto">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <div class="card">
-        <div class="card-body">
-            @if(session()->has('success'))
+        <div class="card">
+            <div class="card-body">
+                @if(session()->has('success'))
                 <div class="alert alert-success alert-dismissible fade show" role="alert">
                     {{ session()->get('success') }}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-            @elseif(session()->has('error'))
+                @elseif(session()->has('error'))
                 <div class="alert alert-danger alert-dismissible fade show" role="alert">
                     {{ session()->get('error') }}
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
+                        <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-            @endif
-            <div class="table-responsive">
-                <table class="table table-bordered" id="dataTables" width="100%" cellspacing="0">
-                    <thead>
-                        <th class="w-15">Item Code</th>
-                        <th>Item Name</th>
-                        <th>Purchase Price</th>
-                        <th>Sale Price</th>
-                        <th>Quantity</th>
-                        <th>Inventory Value</th>
-                        <th>Actions</th>
-                    </thead>
-                    <tbody>
-                        @foreach($inventories as $inventory)
-                        <tr>
-                            <td class="table-item-content">{{ $inventory->item_code }}</td>
-                            <td class="table-item-content">{{ $inventory->item_name }}</td>
-                            <td class="table-item-content">Birr {{  $inventory->purchase_price }}</td>
-                            <td class="table-item-content">Birr {{ $inventory->sale_price }}</td>
-                            <td class="table-item-content">
-                                @if($inventory->inventory_type != 'non_inventory_item')
+                @endif
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTables" width="100%" cellspacing="0">
+                        <thead>
+                            <th class="w-15">Item Code</th>
+                            <th>Item Name</th>
+                            <th>Purchase Price</th>
+                            <th>Sale Price</th>
+                            <th>Quantity</th>
+                            <th>Inventory Value</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                            @foreach($inventories as $inventory)
+                            <tr>
+                                <td class="table-item-content">{{ $inventory->item_code }}</td>
+                                <td class="table-item-content">{{ $inventory->item_name }}</td>
+                                <td class="table-item-content">Birr {{ $inventory->purchase_price }}</td>
+                                <td class="table-item-content">Birr {{ $inventory->sale_price }}</td>
+                                <td class="table-item-content">
+                                    @if($inventory->inventory_type != 'non_inventory_item')
                                     {{ $inventory->quantity }}
-                                @endif
-                            </td>
-                            <td class="table-item-content">
-                                @if($inventory->inventory_type != 'non_inventory_item')
+                                    @endif
+                                </td>
+                                <td class="table-item-content">
+                                    @if($inventory->inventory_type != 'non_inventory_item')
                                     {{ $inventory->inventoryValue }}
-                                @endif
-                            </td>
-                            <td>
-                                <!-- edit -->
-                                <a href="{{ url('inventory/'.$inventory->id.'/edit') }}" class="btn btn-sm btn-primary">
-                                    <span class="icon text-white-50">
-                                        <i class="fas fa-edit"></i>
-                                    </span>
-                                </a>
-                                <!-- delete -->
-                                <button type="button" class="btn btn-sm btn-danger" disabled>
-                                    <span class="icon text-white-50">
-                                        <i class="fas fa-trash"></i>
-                                    </span>
-                                </button>
-                            </td>
+                                    @endif
+                                </td>
+                                <td>
+                                    <!-- edit -->
+                                    <a href="{{ url('inventory/'.$inventory->id.'/edit') }}"
+                                        class="btn btn-sm btn-primary">
+                                        <span class="icon text-white-50">
+                                            <i class="fas fa-edit"></i>
+                                        </span>
+                                    </a>
+                                    <!-- delete -->
+                                    <button type="button" class="btn btn-sm btn-danger" disabled>
+                                        <span class="icon text-white-50">
+                                            <i class="fas fa-trash"></i>
+                                        </span>
+                                    </button>
+                                </td>
 
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 
 
-</main>
+    <div class="col-xl-2 col-lg-3 d-none d-lg-block">
+        <div class="card border-left-success shadow py-2">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                            Total Inventory Value</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">Birr {{ $inventoryValue }}</div>
+                    </div>
+                    <div class="col-auto">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- Modals --}}
+
+{{-- New inventory modal --}}
+<div class="modal fade" id="modal-new-item" tabindex="-1" role="dialog" aria-labelledby="modal-newItem-label"
+    aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modal-receipt-label">New Inventory Item</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            @include('inventory.forms.newItem')
+        </div>
+    </div>
+</div>
 
 <script>
-$(document).ready(function() {
+    $(document).ready(function() {
     $('#dataTables').DataTable();
     $('.dataTables_filter').addClass('pull-right');
 });
