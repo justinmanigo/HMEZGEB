7b93a251fba87330b796a1a3bbe17c7452926168
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-08-15 09:57:06 +0800

    Implement `update()` accounting periods

--- app/Http/Controllers/Settings/AccountingPeriodsController.php
+++ app/Http/Controllers/Settings/AccountingPeriodsController.php
@@ -35,5 +35,23 @@ class AccountingPeriodsController extends Controller
 
     public function updateAjax(UpdateAccountingPeriodsRequest $request)
     {
+        for($i = 0; $i < 12; $i++)
+        {
+            $accounting_period = AccountingPeriods::where('accounting_system_id', session('accounting_system_id'))
+                ->where('period_number', $i+1)
+                ->first();
+            $accounting_period->date_from = $request->date_from[$i];
+            $accounting_period->date_to = $request->date_to[$i];
+            
+            if($accounting_period->isDirty())
+            {
+                $accounting_period->accounting_system_user_id = session('accounting_system_user_id');
+                $accounting_period->save();
+            }
+        }
+
+        return response()->json([
+            'success' => true,
+        ]);
     }
 }
