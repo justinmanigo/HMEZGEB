0ae7d54d029acbdafb0384957c9035594c9f7b0e
Patrick <patrickvelasco31@gmail.com>
2022-08-17 02:17:06 +0800

    Refine mail: bank>transfer

--- resources/views/banking/transfers/mail.blade.php
+++ resources/views/banking/transfers/mail.blade.php
@@ -1,13 +1,16 @@
 @component('mail::message')
-Good day,$bank_transfer
+Good day,
 
 This is your bank transfer details.
 
 @component('mail::table')
-|Transfer Date | Transfer Amount | Transfer Status | From Account | To Account |
-|:--------|:-------------|:-------|:---------| :-------|:-------| 
-| {{date_format($bank_transfer['created_at'],"Y/m/d");}} | {{$bank_transfer['amount']}} | {{$bank_transfer['status']}} | {{$bank_transfer['fromAccount']->chartOfAccount->account_name}} | {{$bank_transfer['toAccount']->chartOfAccount->account_name}} | 
-
+| Name | Value |
+|:------------- |:------------- |
+|Transfer Date | {{date_format($bank_transfer->created_at,"Y/m/d")}} |
+|Transfer Amount | {{$bank_transfer->amount}} |
+|Transfer Status | {{$bank_transfer->status}} |
+|From Account | {{$bank_transfer->fromAccount->chartOfAccount->account_name}} |
+|To Account | {{$bank_transfer->toAccount->chartOfAccount->account_name}} |
 @endcomponent
 
 Thanks,<br>
