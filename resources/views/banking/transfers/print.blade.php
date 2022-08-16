03b0a4629711fa1054f0e8b5780f8178bb1b0310
Patrick <patrickvelasco31@gmail.com>
2022-08-17 02:23:51 +0800

    Refine print: bank>transfer

--- resources/views/banking/transfers/print.blade.php
+++ resources/views/banking/transfers/print.blade.php
@@ -11,10 +11,6 @@
             page-break-after: always;
         }
 
-        .text-center {
-            text-align: center;
-        }
-
         table {
             border-collapse: collapse;
             width: 100%;
@@ -27,20 +23,30 @@
     <table class="text-center">
         <thead>
             <tr>
-                <th>From Account</th>
-                <th>To Account</th>
-                <th>Amount</th>
-                <th>Date</th>
-                <th>Status</th>
+                <th>Name</th>
+                <th>Value</th>
             </tr>
         </thead>
         <tbody>
                 <tr>
-                    <td>{{ $transfers->fromAccount->chartOfAccount->account_name }}</td>
-                    <td>{{ $transfers->toAccount->chartOfAccount->account_name }}</td>
-                    <td>{{ $transfers->amount }}</td>
-                    <td>{{ date_format($transfers->created_at, "Y-m-d") }}</td>
-                    <td>{{ $transfers->status }}</td>
+                    <td>Transfer Date</td>
+                    <td>{{date_format($bank_transfer->created_at,"Y/m/d")}}</td>
+                </tr>
+                <tr>
+                    <td>Transfer Amount</td>
+                    <td>{{$bank_transfer->amount}}</td>
+                </tr>
+                <tr>
+                    <td>Transfer Status</td>
+                    <td>{{$bank_transfer->status}}</td>
+                </tr>
+                <tr>
+                    <td>From Account</td>
+                    <td>{{$bank_transfer->fromAccount->chartOfAccount->account_name}}</td>
+                </tr>
+                <tr>
+                    <td>To Account</td>
+                    <td>{{$bank_transfer->toAccount->chartOfAccount->account_name}}</td>
                 </tr>
         </tbody>
     </table>
