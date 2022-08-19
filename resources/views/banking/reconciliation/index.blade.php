e21226e1ff9c3d24a7e067ef6c5d675d6cecc921
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-08-19 15:47:42 +0800

    [Reconciliation] Create UI for Bank Reconciliation Proper

--- resources/views/banking/reconciliation/index.blade.php
+++ resources/views/banking/reconciliation/index.blade.php
@@ -36,7 +36,219 @@
             </div>
         </div>        
     </div>
+    <div class="row mb-4">
+        <div class="card col-12">
+            <div class="card-body">
+                <h4 class="mb-0">Bank Account Name</h4>
+                <p>Account Number</p>
+                <p>Bank Reconcilation as of <strong>August 31, 2022</strong>
+                <hr>
+                <nav>
+                    <div class="nav nav-pills" id="nav-tab" role="tablist">
+                        <a class="nav-item nav-link active" id="nav-bank-statement-tab" data-toggle="tab" href="#nav-bank-statement" role="tab" aria-controls="nav-bank-statement" aria-selected="true">Bank Statement</a>
+                        <a class="nav-item nav-link" id="nav-cash-book-tab" data-toggle="tab" href="#nav-cash-book" role="tab" aria-controls="nav-cash-book" aria-selected="false">Cash Book</a>
+                        <a class="nav-item nav-link" id="nav-status-tab" data-toggle="tab" href="#nav-status" role="tab" aria-controls="nav-status" aria-selected="false">Status</a>
+                        </div>
+                    </nav>
+                    <hr>
+                    <div class="tab-content" id="nav-tabContent">
+                        
+                        <div class="tab-pane fade show active" id="nav-bank-statement" role="tabpanel" aria-labelledby="nav-bank-statement-tab">
+                            <div class="table-responsive">
+                                <table class="table table-sm table-bordered">
+                                    <thead>
+                                        <th>Date</th>
+                                        <th>Description</th>
+                                        <th>Debit</th>
+                                        <th>Credit</th>
+                                        <th>Balance</th>
+                                        <th>Actions</th>
+                                        <th>OK?</th>
+                                    </thead>
+                                    <div class="tbody">
+                                        <tr>
+                                            <td>August 31, 2022</td>
+                                            <td>Bank Statement</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                            <td>
+                                                <button data-id="1" data-type="bank-statement" class="btn btn-sm btn-primary btn-correct-and-match" data-toggle="modal" data-target="#correctAndMatch">Correct and Match</button>
+                                                <a href="#" class="btn btn-sm btn-info">Create Transaction</a>
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-sm btn-success">OK</a>
+                                            </td>
+                                        </tr>
+                                    </div>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-cash-book" role="tabpanel" aria-labelledby="nav-cash-book-tab">
+                            <div class="table-responsive">
+                                <table class="table table-sm table-bordered">
+                                    <thead>
+                                        <th>Date</th>
+                                        <th>Description</th>
+                                        <th>Debit</th>
+                                        <th>Credit</th>
+                                        <th>Balance</th>
+                                        <th>Actions</th>
+                                        <th>OK?</th>
+                                    </thead>
+                                    <div class="tbody">
+                                        <tr>
+                                            <td>August 31, 2022</td>
+                                            <td>Bank Statement</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                            <td>0.00</td>
+                                            <td>
+                                                <a href="#" class="btn btn-sm btn-primary">Correct and Match</a>
+                                                <a href="#" class="btn btn-sm btn-info">Create Transaction</a>
+                                            </td>
+                                            <td>
+                                                <a href="#" class="btn btn-sm btn-success">OK</a>
+                                            </td>
+                                        </tr>
+                                    </div>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="nav-status" role="tabpanel" aria-labelledby="nav-contact-tab">
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <div class="table-responsive">
+                                        <table class="table table-sm table-bordered">
+                                            <thead>
+                                                <th>Bank Statement</th>
+                                                <th>Debit</th>
+                                                <th>Credit</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Unadjusted Closing Balance</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                            </tbody>
+                                            <tr>
+                                                <td colspan="3"></td>
+                                            </tr>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Add Deposits in Transit</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deduct Outstanding Cheques</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Add / Deduct Bank Errors</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td colspan="3"><span class="text-white">.</span></td>
+                                                </tr>
+                                                <tr>
+                                                    <td colspan="3"><span class="text-white">.</span></td>
+                                                </tr>
+                                            </tbody>
+                                            <tr>
+                                                <td colspan="3"></td>
+                                            </tr>
+                                            <tfoot>
+                                                <th>Adjusted Closing Balance</th>
+                                                <th class="text-right">0.00</th>
+                                                <th class="text-right">0.00</th>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-lg-6">
+                                    <div class="table-responsive">
+                                        <table class="table table-sm table-bordered">
+                                            <thead>
+                                                <th>Cash Book</th>
+                                                <th>Debit</th>
+                                                <th>Credit</th>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Unadjusted Closing Balance</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                            </tbody>
+                                            <tr>
+                                                <td colspan="3"></td>
+                                            </tr>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Add Missing Receipts</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Add Interest Received</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deduct Bank Fees</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deduct Bounced Cheques</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Add / Deduct Errors in Cash Book</td>
+                                                    <td class="text-right">0.00</td>
+                                                    <td class="text-right">0.00</td>
+                                                </tr>
+                                            </tbody>
+                                            <tr>
+                                                <td colspan="3"></td>
+                                            </tr>
+                                            <tfoot>
+                                                <th>Adjusted Closing Balance</th>
+                                                <th class="text-right">0.00</th>
+                                                <th class="text-right">0.00</th>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-12 col-lg-6">
+                                    <div class="table">
+                                        <table class="table table-sm table-bordered">
+                                            <thead>
+                                                <th>Unreconciled Amount</th>
+                                                <th class="text-right">0.00</th>
+                                                <th class="text-right">0.00</th>
+                                            </thead>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
 </div>
+
+</div>
+
 @endsection
 
 @push('scripts')
