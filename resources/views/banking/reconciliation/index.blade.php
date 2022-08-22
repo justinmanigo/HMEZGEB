29e6fdecaf81d41b057a7f88f9f79a0fc25c1142
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-08-22 09:35:03 +0800

    Reduce create transaction modal width to `modal-lg`

--- resources/views/banking/reconciliation/index.blade.php
+++ resources/views/banking/reconciliation/index.blade.php
@@ -360,7 +360,7 @@
 
 <!-- Create Transaction -->
 <div class="modal fade" id="createAndMatch" tabindex="-1" role="dialog" aria-labelledby="createAndMatchLabel" aria-hidden="true">
-    <div class="modal-dialog modal-xl" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="createAndMatchLabel">Create Transaction</h5>
