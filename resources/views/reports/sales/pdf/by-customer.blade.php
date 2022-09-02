c647339ee622ea3ed2486e91ca30329a4195d5bb
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-09-02 17:17:53 +0800

    Add `total_transactions` in Sales by Customer Report

--- resources/views/reports/sales/pdf/by-customer.blade.php
+++ resources/views/reports/sales/pdf/by-customer.blade.php
@@ -3,21 +3,25 @@
 @section('content')
 
 @php
+    $grand_total_transactions = 0;
     $grand_total_amount = 0;
 @endphp
 
 <table class="table table-bordered">
     <thead>
         <th class="text-start">Customer</th>
+        <th class="text-end">Total Transactions</th>
         <th class="text-end">Total Amount</th>
     </thead>
     <tbody>
     @foreach($results as $row)
         <tr>
             <td class="text-start">{{ $row->customer }}</td>
+            <td class="text-end">{{ number_format($row->total_transactions) }}</td>
             <td class="text-end">{{ number_format($row->total_amount, 2) }}</td>
         </tr>
         @php
+            $grand_total_transactions += $row->total_transactions;
             $grand_total_amount += $row->total_amount;
         @endphp
     @endforeach
@@ -25,6 +29,9 @@
 
     <tfoot>
         <th class="text-start" colspan="1">Grand Total</th>
+        <th class="text-end" style="color:darkgreen">
+            {{ number_format($grand_total_transactions) }}
+        </th>
         <th class="text-end" style="color:darkgreen">
             {{ number_format($grand_total_amount, 2) }}
         </th>
