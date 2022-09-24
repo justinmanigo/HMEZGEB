aa99dacd5fef28c38848797bb13cc86921863d38
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-09-24 17:30:21 +0800

    Add tooltip indicating that linked credit receipts will also be marked void

--- resources/views/customer/receipt/index.blade.php
+++ resources/views/customer/receipt/index.blade.php
@@ -696,7 +696,7 @@ POTENTIAL SOLUTIONS:
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Are you sure you want to void record?</p>
+                    <p>Are you sure you want to void record? Credit Receipts linked to this receipt will also be marked void.</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
