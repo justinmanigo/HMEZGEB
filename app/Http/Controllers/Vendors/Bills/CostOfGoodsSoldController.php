bf58c3799f70a209b6cac618ad234c14a1d59a56
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-11-01 09:56:55 +0800

    Remove `Request` in list of used classes in COGSController

--- app/Http/Controllers/Vendors/Bills/CostOfGoodsSoldController.php
+++ app/Http/Controllers/Vendors/Bills/CostOfGoodsSoldController.php
@@ -6,7 +6,6 @@ use App\Actions\CreateJournalEntry;
 use App\Actions\CreateJournalPostings;
 use App\Actions\Customer\Receipt\DetermineReceiptStatus;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\Vendors\Bills\StoreCostOfGoodsSoldRequest;
 use App\Models\PaymentReferences;
 use Illuminate\Support\Facades\DB;
