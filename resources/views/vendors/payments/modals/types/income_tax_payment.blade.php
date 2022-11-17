37d84e0a0226ae2441b0cf7774b60f638ad97b95
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-11-17 10:13:55 +0800

    Remove dash between Income Tax in Modal Title

--- resources/views/vendors/payments/modals/types/income_tax_payment.blade.php
+++ resources/views/vendors/payments/modals/types/income_tax_payment.blade.php
@@ -5,7 +5,7 @@
         <div class="modal-dialog modal-xl" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="modal-income-tax-payment-label">New Income-Tax Payment</h5>
+                    <h5 class="modal-title" id="modal-income-tax-payment-label">New Income Tax Payment</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
