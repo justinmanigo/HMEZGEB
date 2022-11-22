71609ef5208182cc8823b46fd81b6c3c9318b973
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-11-22 16:14:21 +0800

    Update UI to match with requirements
    As mentioned in summary and update # 1 ppt
    > To select a Period to pay withholding payment let a user can select 1 period or more than 1 period at a time.

--- resources/views/vendors/payments/modals/types/withholding_payment.blade.php
+++ resources/views/vendors/payments/modals/types/withholding_payment.blade.php
@@ -18,28 +18,6 @@
                                 <input class="col-8 col-lg-7 form-control" id="wp_cash_account" name='cash_account'>
                                 <p class="col-8 col-lg-5 text-danger error-message error-message-cash_account" style="display:none"></p>
                             </div>
-                            <div class="form-group row">
-                                <label for="wp_accounting_period" class="col-4 col-form-label text-left">Accounting Period<span class="text-danger ml-1">*</span> :</label>
-                                <input class="col-8 col-lg-7 form-control" id="wp_accounting_period" name='accounting_period'>
-                                <p class="col-8 col-lg-5 text-danger error-message error-message-accounting_period" style="display:none"></p>
-                            </div>
-                            {{-- Contact Details --}}
-                            <div class="table-responsive">
-                                <table class="table-sm">
-                                    <tr>
-                                        <td width="200px">Current Balance:</td>
-                                        <td width="150px" class="text-right" id="wp_cash_account_current_balance">0.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Withholding Amount:</td>
-                                        <td width="150px" class="text-right" id="wp_withholding_amount">0.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>Balance After:</td>
-                                        <td width="150px" class="text-right" id="wp_cash_account_balance_after_transaction">0.00</td>
-                                    </tr>
-                                </table>
-                            </div>
                         </div>
 
                         <div class="col-12 col-lg-6">
@@ -60,6 +38,50 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="row mb-3">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <th>Period Number</th>
+                                    <th>Date Range</th>
+                                    <th>Status</th>
+                                    <th class="text-right">Total Withholding</th>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            {{--Checkbox--}}
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="wp_period_1" value="1">
+                                                <label class="custom-control-label" for="wp_period_1">Period # 1</label>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            2022-01-01 to 2022-01-31
+                                        </td>
+                                        <td>
+                                            <span class="badge badge-success">Paid</span>
+                                        </td>
+                                        <td class="text-right">
+                                            1000.00
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <th>
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="wp_select_all" value="1">
+                                            <label class="custom-control-label" for="wp_select_all">Select All</label>
+                                        </div>
+                                    </th>
+                                    <th colspan="2" class="text-right">Total Withholding to Pay</th>
+                                    <th class="text-right"><strong>0.00</strong></th>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+
                     <div class="form-group row">
                         <label for="name" class="col-md-4 col-lg-2 col-form-label">Remark:</label>
                         <textarea name="remark" class="form-control col-lg-4"></textarea>
