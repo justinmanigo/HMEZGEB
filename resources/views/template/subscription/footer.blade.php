7e9780b8262460b6295fc57f6696577d25796670
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-12-06 03:03:13 +0800

    Dynamically change copyright year
    Since the new year is already approaching :D

--- resources/views/template/subscription/footer.blade.php
+++ resources/views/template/subscription/footer.blade.php
@@ -3,7 +3,7 @@
             <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; HMEZGEB 2022</span>
+                        <span>Copyright &copy; HMEZGEB 2022-{{ date('Y') }}</span>
                     </div>
                 </div>
             </footer>
