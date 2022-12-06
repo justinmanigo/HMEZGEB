a0f3e77d1ef095400a296e73396e638de64cc239
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-12-07 04:20:57 +0800

    Remove unused code in BillsController

--- app/Http/Controllers/BillsController.php
+++ app/Http/Controllers/BillsController.php
@@ -84,24 +84,6 @@ class BillsController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-
-
     // Void
     public function voidBill($id)
     {
@@ -204,40 +186,4 @@ class BillsController extends Controller
         $purchaseOrders = PurchaseOrders::find($id);
         return view('vendors.bills.purchase_order.show', compact('purchaseOrders'));
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Bills  $bills
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Bills $bills)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Bills  $bills
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Bills $bills)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Bills  $bills
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Bills $bills)
-    {
-        //
-    }
-
-
 }
