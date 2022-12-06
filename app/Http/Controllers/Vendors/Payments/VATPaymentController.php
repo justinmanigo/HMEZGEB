6f2460adae17f8fe475b3a7167e2553d24b24132
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-12-07 04:56:56 +0800

    Add blank `store()` method in CommissionPayment and VATPayment Controllers

---
+++ app/Http/Controllers/Vendors/Payments/VATPaymentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Vendors\Payments;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VATPaymentController extends Controller
+{
+    /**
+     * TODO: to implement
+     *
+     * Stores a new entry.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+}
