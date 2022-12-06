056939781b80562b26b3b29b8a1b85de895371e1
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-12-07 04:47:52 +0800

    Remove unused code in PaymentsController

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -117,27 +117,6 @@ class PaymentsController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
     public function storeIncomeTaxPayment(Request $request)
     {
         $status = 'paid';
