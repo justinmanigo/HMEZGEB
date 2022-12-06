8a734f3fd7b2727890d31ed8a06a3d778c48ed49
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2022-12-07 04:49:43 +0800

    Remove old income tax payment code

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -117,33 +117,6 @@ class PaymentsController extends Controller
         ]);
     }
 
-    public function storeIncomeTaxPayment(Request $request)
-    {
-        $status = 'paid';
-
-        // Store payment reference
-        $reference = PaymentReferences::create([
-            'accounting_system_id' => $this->request->session()->get('accounting_system_id'),
-            'vendor_id' => $request->vendor_id,
-            'date' => $request->date,
-            'type' => 'income_tax_payment',
-            'attachment' => isset($fileAttachment) ? $fileAttachment : null,
-            'remark' => $request->remark,
-            'status' => $status
-        ]);
-
-        // store income tax payment
-        $incomeTaxPayment = IncomeTaxPayments::create([
-            'payment_reference_id' => $reference->id,
-            'accounting_period_id' => $request->accounting_period_id,
-            'chart_of_account_id' => $request->chart_of_account_id,
-            'cheque_number' => $request->cheque_number,
-            'amount_received' => $request->amount_received,
-        ]);
-
-        return redirect()->back()->with('success', 'Income Tax Payment has been added successfully.');
-    }
-
     // Store pension
     public function storePensionPayment(Request $request)
     {
