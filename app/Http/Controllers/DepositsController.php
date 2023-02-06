af1861cb382ce832c5b832a1d85eb9eb1288c25e
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2023-02-06 09:34:19 +0800

    Fix code indentation in ajaxSearchBank function

--- app/Http/Controllers/DepositsController.php
+++ app/Http/Controllers/DepositsController.php
@@ -150,32 +150,32 @@ class DepositsController extends Controller
 
      /******* AJAX ***********/
 
-     public function ajaxSearchBank($query)
-     {
-         return ChartOfAccounts::select(
-                 'chart_of_accounts.id as value',
-                 'chart_of_accounts.chart_of_account_category_id',
-                 'chart_of_accounts.chart_of_account_no',
-                 'chart_of_accounts.account_name',
-                 'bank_accounts.bank_branch',
-                 'bank_accounts.bank_account_number',
-                 'bank_accounts.bank_account_type',
-                 'chart_of_account_categories.category',
-                 'chart_of_account_categories.normal_balance',
-             )
-             ->leftJoin('chart_of_account_categories', 'chart_of_accounts.chart_of_account_category_id', '=', 'chart_of_account_categories.id')
-             ->leftJoin('bank_accounts', 'chart_of_accounts.id', '=', 'bank_accounts.chart_of_account_id')
-             ->where('chart_of_account_categories.category' , '=', 'Cash')
-             ->where('bank_accounts.bank_account_number' , '!=', NULL)
-             ->where('chart_of_accounts.accounting_system_id', session('accounting_system_id'))
-             ->where(function($sql) use ($query) {
-                 $sql->where('chart_of_accounts.account_name', 'LIKE', "%{$query}%")
-                     ->orWhere('chart_of_accounts.chart_of_account_no', 'LIKE', "%{$query}%")
-                     ->orWhere('bank_accounts.bank_account_number', 'LIKE', "%{$query}%")
-                     ->orWhere('bank_accounts.bank_branch', 'LIKE', "%{$query}%");
-             })
-             ->get();
-     }
+    public function ajaxSearchBank($query)
+    {
+        return ChartOfAccounts::select(
+                'chart_of_accounts.id as value',
+                'chart_of_accounts.chart_of_account_category_id',
+                'chart_of_accounts.chart_of_account_no',
+                'chart_of_accounts.account_name',
+                'bank_accounts.bank_branch',
+                'bank_accounts.bank_account_number',
+                'bank_accounts.bank_account_type',
+                'chart_of_account_categories.category',
+                'chart_of_account_categories.normal_balance',
+            )
+            ->leftJoin('chart_of_account_categories', 'chart_of_accounts.chart_of_account_category_id', '=', 'chart_of_account_categories.id')
+            ->leftJoin('bank_accounts', 'chart_of_accounts.id', '=', 'bank_accounts.chart_of_account_id')
+            ->where('chart_of_account_categories.category' , '=', 'Cash')
+            ->where('bank_accounts.bank_account_number' , '!=', NULL)
+            ->where('chart_of_accounts.accounting_system_id', session('accounting_system_id'))
+            ->where(function($sql) use ($query) {
+                $sql->where('chart_of_accounts.account_name', 'LIKE', "%{$query}%")
+                    ->orWhere('chart_of_accounts.chart_of_account_no', 'LIKE', "%{$query}%")
+                    ->orWhere('bank_accounts.bank_account_number', 'LIKE', "%{$query}%")
+                    ->orWhere('bank_accounts.bank_branch', 'LIKE', "%{$query}%");
+            })
+            ->get();
+    }
 
     public function ajaxGetUndepositedReceipts()
     {
