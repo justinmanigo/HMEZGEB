baddb8bdfb6c085180350f6c90b3b6e11623774a
Xyber Pastoril <32955000+xyberpastoril@users.noreply.github.com>
2023-02-08 09:46:12 +0800

    Use `session('accounting_system_id')` instead of relying on request variable

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -23,8 +23,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
-        $customers = Customers::where('accounting_system_id', $accounting_system_id)->get();
+        $customers = Customers::where('accounting_system_id', session('accounting_system_id'))->get();
         
         $total_balance = 0;
         $total_balance_overdue = 0;
@@ -58,15 +57,13 @@ class CustomerController extends Controller
      */
     public function store(Request $request)
     {   
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
-
         if($request->image) {
             $imageName = time().'.'.$request->image->extension();  
             $request->image->storeAs('customers', $imageName);
         }
         
         $customers = new Customers();
-        $customers->accounting_system_id = $accounting_system_id;
+        $customers->accounting_system_id = session('accounting_system_id');
         $customers->name =  $request->name;
         $customers->tin_number =  $request->tin_number;
         $customers->address =  $request->address;
@@ -95,16 +92,14 @@ class CustomerController extends Controller
      */
     public function show(Customers $customers)
     {
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
-        $customers = Customers::where('accounting_system_id', $accounting_system_id)->get();
+        $customers = Customers::where('accounting_system_id', session('accounting_system_id'))->get();
 
         return view('customer.customer.index',compact('customers'));
     }
     public function edit($id)
     {
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
         $customers = Customers::find($id);
-        if($customers->accounting_system_id != $accounting_system_id)
+        if($customers->accounting_system_id != session('accounting_system_id'))
             return redirect()->route('cutomer.cutomer.index')->with('danger', "You are not authorized to edit this customer.");
 
         return view('customer.customer.edit', compact('customers'));
@@ -180,8 +175,7 @@ class CustomerController extends Controller
     // Specific customer mail
     public function mailCustomerStatement($id)
     {
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
-        $customer = Customers::where('accounting_system_id', $accounting_system_id)
+        $customer = Customers::where('accounting_system_id', session('accounting_system_id'))
         ->where('id', $id)
         ->whereHas('receiptReference', function($query) {
             $query->where('type', 'receipt')
@@ -277,9 +271,8 @@ class CustomerController extends Controller
 
     public function queryCustomers($query)
     {   
-        $accounting_system_id = $this->request->session()->get('accounting_system_id');
         $customers = Customers::select('id as value', 'name', 'tin_number', 'contact_person','mobile_number')
-            ->where('accounting_system_id', $accounting_system_id)
+            ->where('accounting_system_id', session('accounting_system_id'))
             ->where('name', 'LIKE', '%' . $query . '%')->get();
             
         return $customers;
